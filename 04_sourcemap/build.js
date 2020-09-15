563f5370809ed115e761a902e308965039b60ff8
mima3 <7877000+mima3@users.noreply.github.com>
2020-09-15 21:26:22 +0900

    web-dev-serverの実行例を追加

--- 04_sourcemap/build.js
+++
@@ -1,7 +0,0 @@
-const config = {}; //require('./webpack.config.js');
-const webpack = require('webpack');
-
-webpack(config, (err, stats) => {
-  console.error(err);
-  console.error(stats);
-});
---
+++ 05_dev_server/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: path.join(__dirname, "src", "page1_src.js"),
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "page1.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public")
+  }
+
+}
